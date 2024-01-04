4722dad93c1d9cf583b4405aa6a737573ae72ace
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-01-04 18:53:59 +0600

    Leetcode 763. Partition Labels

---
+++ 763. Partition Labels/code.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        char[] arr = s.toCharArray();
+        int[] cache = new int[128];
+
+        for(int i=0; i<arr.length; i++){
+            char cur = arr[i];
+            cache[cur] = i;
+        }
+        int L=0, R=0, index=0;
+        List<Integer> res = new LinkedList<>();
+
+        while(index < arr.length){
+            char cur = arr[index];
+            R = Math.max(R, cache[cur]);
+
+            if(R == index){
+                int size = R-L+1;
+                res.add(size);
+                R++;
+                L=R;
+            }
+            index++;
+        }
+        return res;
+    }
+}
